691260193fc3f4aa6e31f9209f5951b5cda42c3c
issam <issamriko9@gmail.com>
2024-03-21 21:11:58 +0100

    create splash view body

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookme/features/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const Bookme());
+}
+
+class Bookme extends StatelessWidget {
+  const Bookme({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
